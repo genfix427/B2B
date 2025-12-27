fc35c672d9c81601edcd83065e4b59eed64a9ebc
abdul8840 <abdul14941naseer@gmail.com>
2025-12-27 21:00:46 +0530

    Create home page components

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Hero from '../components/HomeContent/Hero'
+import WhyChooseUs from '../components/HomeContent/WhyChooseUs'
+import AboutUsHome from '../components/HomeContent/AboutUsHome'
+import Testimonials from '../components/TestimonialsContent/Testimonials'
+import FaqSection from '../components/HomeContent/FaqSection'
+import JoinBanner from '../components/HomeContent/JoinBanner'
 
 const Home = () => {
   return (
-    <div className='h-[150vh] bg-amber-200'>
-      Home Page
+    <div className="mt-12">
+      <Hero />
+      <WhyChooseUs />
+      <AboutUsHome />
+      <Testimonials />
+      <FaqSection />
+      <JoinBanner />
     </div>
   )
 }
